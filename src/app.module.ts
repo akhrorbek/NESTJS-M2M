a29ac2ff4f9b807c3528af8ff251d35f7f60dbe5
akhrorbek <akhrornarzullayev.na@gmail.com>
2023-03-03 17:29:58 +0500

    init:update/logic

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,9 @@
 import { Module } from '@nestjs/common';
 import { ConfigModule } from '@nestjs/config/dist/config.module';
 import { config } from './config';
-import { CategoryModule } from './module/category/category.module';
-import { PostModule } from './module/post/post.module';
-
+import { JobModule } from './module/job/job.module';
+import { EmployeesModule } from './module/employees/employees.module';
 @Module({
-  imports: [ConfigModule.forRoot(config),CategoryModule, PostModule,]
+  imports: [ConfigModule.forRoot(config), JobModule, EmployeesModule],
 })
 export class AppModule {}
